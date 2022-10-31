74a87cead23d32e094f17756c5f80b38688d166d
Nurlan Memmedov <nurlan.memmedov8818@gmail.com>
2022-10-31 23:57:59 +0400

    Loops Task is redy

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿//Task1
+//int userNumber = 5;
+//int factoryal = 1;
+//for (int i = 1; i <= userNumber; i++)
+//{
+//    factoryal *= i; //factoryal = factoryal * i;
+//}
+//Console.WriteLine(factoryal);
+
+//TAsk2
+//int userNumber = 1;
+//int count = 0;
+//for (int i = 1; i <= userNumber; i++)
+//{
+//    if (userNumber % i == 0)
+//    {
+//        count++;
+
+//    }
+//}
+//if (userNumber == 1)
+//{
+//    Console.WriteLine("1 ne sadedir ne murekkeb");
+//}
+// else if (count > 2)
+//{
+//    Console.WriteLine("eded murekkebdir");
+//}
+//else
+//{
+//    Console.WriteLine("Eded sadedir");
+//}
+
+//Task3
+//int number = 12345;
+//int count = 0;
+//for (int i = 0; i < number; i++)
+//{
+//    number /= 10;
+//    if (number > 0)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count + 1);
---
+++ scriot.js
@@ -0,0 +1,45 @@
+//Task1
+// var userNumber = 5;
+// var factoryal = 1;
+// for (var i = 1; i <= userNumber; i++)
+// {
+//     factoryal *= i; //factoryal = factoryal * i;
+// }
+// Console.log(factoryal);
+
+//Task2
+// var userNumber = 7;
+// var count = 0;
+// for (i = 1; i <= userNumber; i++)
+// {
+//    if (userNumber % i == 0)
+//    {
+//        count++;
+
+//    }
+// }
+// if (userNumber == 1)
+// {
+//    console.log("1 ne sadedir ne murekkeb");
+// }
+// else if (count > 2)
+// {
+//    console.log ("eded murekkebdir");
+// }
+// else
+// {
+//    console.log("Eded sadedir");
+// }
+
+//Task3
+//var number = 123;
+//var count = 0;
+//for (var i = 0; i < number; i++)
+//{
+//    number /= 10;
+//    if (number > 0)
+//    {
+//        count++;
+//    }
+//}
+//console.log(count + 1);
